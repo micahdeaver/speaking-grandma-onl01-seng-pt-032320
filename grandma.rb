6190d70541901201307bd50363dab2afd6ce31b0
Micah Deaver <micahdeaver@me.com>
2020-03-26 00:17:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase.downcase 
+  else phrase.downcase 
     "HUH?! SPEAK UP, SONNY!"
   end
 end
